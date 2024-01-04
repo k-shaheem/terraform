5cc29968814cc8c17f8d93a6908ed7af16ef49af
shaheem khan <shaheemkhan928@gmail.com>
2024-01-04 16:03:31 +0530

    modified main.tf file

--- main.tf
+++ main.tf
@@ -7,6 +7,11 @@ terraform {
   }
 }
 
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAZ4Z4KAC33ZLKYAZK"
+  secret_key = "=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"
+}
 
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
